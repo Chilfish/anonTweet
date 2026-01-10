8d20f4157d521e354c9b53527e457beada132b2d
Chilfish <chill4fish@gmail.com>
2026-01-10 13:31:21 +0800

    fix: disable /user/timeline for public

--- app/routes/api/user/timeline.ts
+++ app/routes/api/user/timeline.ts
@@ -1,23 +1,23 @@
 import type { Route } from './+types/get'
-import type { TweetData } from '~/types'
-import { isProduction } from '~/lib/env.server'
-import { getEnrichedUserTweet } from '~/lib/react-tweet/utils/get-tweet'
-import { getDBUser } from '~/lib/service/getUser.server'
+import { data } from 'react-router'
 
 export async function loader({
   params,
-}: Route.LoaderArgs): Promise<TweetData> {
-  if (!isProduction) {
-    return []
-  }
+}: Route.LoaderArgs) {
+  return data({
+    error: 'Rate limit exceeded',
+    message: '建议在使用RSS轮询的时候，使用自己部署的实例，并用自己的key。请参考项目文档：https://github.com/Chilfish/anonTweet/',
+  }, {
+    status: 429,
+  })
 
-  const { username } = params
+  // const { username } = params
 
-  const user = await getDBUser(username)
+  // const user = await getDBUser(username)
 
-  if (!user?.id) {
-    return []
-  }
+  // if (!user?.id) {
+  //   return []
+  // }
 
-  return await getEnrichedUserTweet(user.id)
+  // return await getEnrichedUserTweet(user.id)
 }
