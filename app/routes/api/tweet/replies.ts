645f988fe08b8dca2edf5f8982bbe4641f898d5c
Chilfish <chill4fish@gmail.com>
2026-01-19 01:07:48 +0800

    fix(build): mark node:* as comments

--- app/routes/api/tweet/replies.ts
+++ app/routes/api/tweet/replies.ts
@@ -1,20 +1,20 @@
 import type { Route } from './+types/replies'
-import type { EnrichedTweet, TweetData } from '~/types'
-import { getLocalCache } from '~/lib/localCache'
+import type { TweetData } from '~/types'
+// import { getLocalCache } from '~/lib/localCache'
 import { enrichTweet } from '~/lib/react-tweet'
 import { fetchReplies } from '~/lib/react-tweet/utils/get-tweet'
 
-function getLocalReplies(tweetId: string) {
-  return getLocalCache<EnrichedTweet[]>({
-    id: tweetId,
-    type: 'replies',
-    getter: () => fetchReplies(tweetId)
-      .then(async (d) => {
-        const enrichedTweets = d.map(t => enrichTweet(t))
-        return enrichedTweets
-      }),
-  })
-}
+// function getLocalReplies(tweetId: string) {
+//   return getLocalCache<EnrichedTweet[]>({
+//     id: tweetId,
+//     type: 'replies',
+//     getter: () => fetchReplies(tweetId)
+//       .then(async (d) => {
+//         const enrichedTweets = d.map(t => enrichTweet(t))
+//         return enrichedTweets
+//       }),
+//   })
+// }
 
 export async function loader({
   params,
