4ab7d32ada504f9f1e1fca12a28199545ca81e90
Chilfish <chill4fish@gmail.com>
2026-04-26 19:36:16 +0800

    fix: 更新 constants.ts 中的预设翻译模板

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -55,7 +55,7 @@ export const DEFAULT_TEMPLATES: SeparatorTemplate[] = [
     id: 'preset-google',
     name: '谷歌翻译风格',
     html: `<div style="margin-top: 4px; color: #1d9bf0;">
-    <b style="font-weight: bold; font-size: small;">由 谷歌 翻译自 日语</b>
+    <b style="font-weight: bold; font-size: small;">由 谷歌 翻译</b>
     <hr style="margin: 3px; border-top-width: 2px;">
   </div>`,
   },
@@ -63,7 +63,7 @@ export const DEFAULT_TEMPLATES: SeparatorTemplate[] = [
     id: 'preset-gemini',
     name: 'Gemini 翻译风格',
     html: `<div style="margin-top: 4px; color: #3285FD;">
-<b style="font-weight: bold; font-size: small;">由 Gemini 2.0 Flash 翻译</b>
+<b style="font-weight: bold; font-size: small;">由 Gemini 翻译</b>
 <hr style="margin: 3px; border-top-width: 2px;">
 </div>`,
   },
