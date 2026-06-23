2caf523d1a7f162aa1a476792fb3244586005ef3
Chilfish <chill4fish@gmail.com>
2026-06-23 22:22:35 +0800

    build: update deps

--- react-router.config.ts
+++ react-router.config.ts
@@ -9,11 +9,6 @@ const config: Config = {
     vercelPreset(),
   ],
   future: {
-    v8_middleware: true,
-    v8_splitRouteModules: true,
-    v8_viteEnvironmentApi: true,
-    v8_passThroughRequests: true,
-    v8_trailingSlashAwareDataRequests: true,
   },
 }
 
